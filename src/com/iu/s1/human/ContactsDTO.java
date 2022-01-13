b25a887efb511af06a91c6c6a295a64aaa1173ad
simDH <jsy670711@gmail.com>
2022-01-13 16:33:23 +0900

    add DTO

---
+++ src/com/iu/s1/human/ContactsDTO.java
@@ -0,0 +1,43 @@
+package com.iu.s1.human;
+
+public class ContactsDTO {
+
+	private String name;		// 이름
+	private String nickName;	// 별명
+	private String email;		// 이메일
+	private String phoneNum;	// 전화번호
+	private String birth;		// 생일
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	
+	
+}
---
+++ src/com/iu/s1/human/ContactsMain.java
@@ -0,0 +1,9 @@
+package com.iu.s1.human;
+
+public class ContactsMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
